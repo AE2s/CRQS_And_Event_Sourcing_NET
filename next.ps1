0303fb18d48d3bb4f915ee4d10f6f9b0be0f36d1
Boris <anas.essaghir@gmail.com>
2020-04-07 20:19:13 +0200

    Add test navigation commands

--- next.ps1
+++ next.ps1
@@ -1,10 +1,8 @@
 git add -A 
 git commit -m "Resolve test"
-git merge step4-test4
+git merge step5-test1
 Write-Host ""
 Write-Host ""
-Write-Host -f green "========================"
-Write-Host -f green "===  STEP 4 - Test 4 ==="
-Write-Host -f green "========================"
+Get-Content stepsDoc/step5.txt | Write-Host -f green
 Write-Host ""
 Write-Host ""
