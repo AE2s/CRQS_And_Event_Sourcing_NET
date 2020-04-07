64bedef1290ccc61fc3bf6c212bc70419b5f9be8
Boris <anas.essaghir@gmail.com>
2020-04-07 20:19:15 +0200

    Add end test navigation commands

--- next.ps1
+++ next.ps1
@@ -1,10 +1,5 @@
-git add -A 
-git commit -m "Resolve test"
-git merge step5-test2
 Write-Host ""
 Write-Host ""
-Write-Host -f green "========================"
-Write-Host -f green "===  STEP 5 - Test 2 ==="
-Write-Host -f green "========================"
+Get-Content stepsDoc/end.txt | Write-Host -f green
 Write-Host ""
 Write-Host ""
