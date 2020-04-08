6c6b169782c93f7b4113fa9bea05cf11832c9269
Boris <anas.essaghir@gmail.com>
2020-04-08 12:01:09 +0200

    Resolve test

--- Mixter.Domain/Core/Subscriptions/Handlers/UpdateFollowers.cs
+++ Mixter.Domain/Core/Subscriptions/Handlers/UpdateFollowers.cs
@@ -1,7 +1,22 @@
-﻿namespace Mixter.Domain.Core.Subscriptions.Handlers
+﻿using Mixter.Domain.Core.Subscriptions.Events;
+using Mixter.Domain.Identity;
+
+namespace Mixter.Domain.Core.Subscriptions.Handlers
 {
     [Handler]
     public class UpdateFollowers
     {
+        private readonly IFollowersRepository _followersRepository;
+
+        public UpdateFollowers(IFollowersRepository followersRepository)
+        {
+            _followersRepository = followersRepository;
+        }
+
+        public void Handle(UserFollowed userFollowed)
+        {
+            var followerProjection = new FollowerProjection(userFollowed.SubscriptionId.Followee, userFollowed.SubscriptionId.Follower);
+            _followersRepository.Save(followerProjection);
+        }
     }
 }
