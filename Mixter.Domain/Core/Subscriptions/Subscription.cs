6f2967893aa864e66a643c2ae4fb52cd2eed0391
Boris <anas.essaghir@gmail.com>
2020-04-08 11:54:18 +0200

    Resolve test

--- Mixter.Domain/Core/Subscriptions/Subscription.cs
+++ Mixter.Domain/Core/Subscriptions/Subscription.cs
@@ -9,32 +9,85 @@ namespace Mixter.Domain.Core.Subscriptions
     [Aggregate]
     public class Subscription
     {
-        private IList<object> subscriptionIds;
+        DecisionProjection _projections = new DecisionProjection();
         public Subscription(IDomainEvent[] events)
         {
-            subscriptionIds=new List<object>();
+
+
             foreach (var @event in events)
             {
-                subscriptionIds.Add(@event.GetAggregateId());
+                _projections.Apply(@event);
             }
         }
 
         public static void FollowUser(IEventPublisher eventPublisher, UserId follower, UserId followee)
         {
-            var suscriptionId=new SubscriptionId(follower, followee);
+            var suscriptionId = new SubscriptionId(follower, followee);
             eventPublisher.Publish(new UserFollowed(suscriptionId));
         }
 
         public void Unfollow(IEventPublisher evt)
         {
-            var id =(SubscriptionId)subscriptionIds.First();
-            evt.Publish(new UserUnfollowed(id));
+           
+            evt.Publish(new UserUnfollowed(_projections.SubscriptionId));
         }
 
         public void NotifyFollower(IEventPublisher evt, MessageId messageId)
         {
-            var id = (SubscriptionId)subscriptionIds.First();
-            evt.Publish(new FolloweeMessageQuacked(id,messageId));
+            if (!_projections.Quackers.Contains(_projections.SubscriptionId))
+                return;
+            evt.Publish(new FolloweeMessageQuacked(_projections.SubscriptionId, messageId));
+        }
+
+        [Projection]
+        private class DecisionProjection : DecisionProjectionBase
+        {
+            private readonly IList<SubscriptionId> _quackers = new List<SubscriptionId>();
+
+            public MessageId MessageId { get; private set; }
+
+            public SubscriptionId SubscriptionId { get; private set; }
+
+            public IEnumerable<SubscriptionId> Quackers
+            {
+                get { return _quackers; }
+            }
+
+            public DecisionProjection()
+            {
+                AddHandler<UserFollowed>(When);
+                AddHandler<UserUnfollowed>(When);
+                AddHandler<FolloweeMessageQuacked>(When);
+            }
+
+            private void When(UserFollowed evt)
+            {
+                SubscriptionId = evt.SubscriptionId;
+
+                _quackers.Add(evt.SubscriptionId);
+            }
+
+            private void When(UserUnfollowed evt)
+            {
+                if (_quackers.Contains(evt.SubscriptionId))
+                {
+                    SubscriptionId = evt.SubscriptionId;
+                    _quackers.Remove(evt.SubscriptionId);
+                }
+            }
+
+            private void When(FolloweeMessageQuacked evt)
+            {
+                if (!_quackers.Contains(evt.SubscriptionId))
+                    return;
+
+                SubscriptionId = evt.SubscriptionId;
+                MessageId = evt.MessageId;
+               
+
+            }
         }
     }
+
+
 }
