25d4004a94e4a25f737a0ab5075cde087e0d3d24
Boris <anas.essaghir@gmail.com>
2020-04-08 10:49:16 +0200

    Resolve test

--- Mixter.Domain/Core/Messages/Handlers/UpdateTimeline.cs
+++ Mixter.Domain/Core/Messages/Handlers/UpdateTimeline.cs
@@ -1,7 +1,21 @@
-﻿namespace Mixter.Domain.Core.Messages.Handlers
+﻿using Mixter.Domain.Core.Messages.Events;
+
+namespace Mixter.Domain.Core.Messages.Handlers
 {
     [Handler]
     public class UpdateTimeline
     {
+        private readonly ITimelineMessageRepository _timelineMessageRepository;
+
+        public UpdateTimeline(ITimelineMessageRepository timelineMessageRepository)
+        {
+            _timelineMessageRepository = timelineMessageRepository;
+        }
+
+        public void Handle(IDomainEvent evt)
+        {
+            var messageQuacked = (MessageQuacked) evt;
+            _timelineMessageRepository.Save(new TimelineMessageProjection(messageQuacked.Author, messageQuacked.Author,messageQuacked.Content,messageQuacked.Id));
+        }
     }
 }
